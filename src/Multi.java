b681547c0b83c026c0265ccbb33335fcfa8949b7
Matías <matiasdim3@gmail.com>
2017-09-24 14:47:39 -0400

    Docs addition. Algorithm now reads input files and calculates matrix

--- src/Multi.java
+++ src/Multi.java
@@ -1,19 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Multi {
-    String tArray[] = {"A", "T", "A", "G", "C", "T"}; //j
-    String sArray[] = {"G", "A", "T", "A", "T", "G", "C", "A"}; //i
-    int [][] matrix = new int[sArray.length+1][tArray.length+1];
+
+    private String s; // To store the sequence read file
+    private String t; // To store the unknown read file
+    private String S_FILE_NAME = "Example1S";//"HIV-1_db.fasta"; // Change to use another sequence input file
+    private String T_FILE_NAME = "Example1T"; //"HIV-1_Polymerase.txt"; // Change to use another unknown input file
+
     int generalMax = 0;
-    // To storeinal value max and positions i,j
+    // To store final max value and positions i,j
     int max = 0;
-    int i = 0;
-    int j = 0;
+    int maxI = 0;
+    int maxJ = 0;
 
-    public void Multi(){
-
+    public void Multi(String t, String s){
+        this.t = t;
+        this.s = s;
+        String tArray[] = t.split("");
+        String sArray[] = s.split("");
     }
 
-    public void calcMatrix(){
-        initMatrix();
+    public void execute(){
+        s = readFile(S_FILE_NAME);
+        t = readFile(T_FILE_NAME);
+        calcMatrix(s.split(""), t.split(""));
+    }
+
+    public void calcMatrix(String[] sArray, String[] tArray){
+        int [][] matrix = initMatrix(sArray, tArray);
         int currentMax = 0;
         for(int i = 1; i < matrix.length; i++){
             for(int j = 1; j < matrix[0].length; j++){
@@ -22,8 +39,12 @@ public class Multi {
                 values[1] = calculateNorthOrWest(matrix[i-1][j]);
                 values[2] = calculateNorthWest(matrix[i-1][j-1], sArray[i-1], tArray[j-1]);
                 currentMax = getMax(values);
-                if (generalMax <= currentMax)
-                    matrix[i][j] =  getMax(values);
+                if (generalMax <= currentMax){
+                    generalMax = currentMax;
+                    maxJ = j;
+                    maxI = i;
+                }
+                matrix[i][j] = currentMax;
             }
         }
         for(int i = 0; i < matrix.length; i++){
@@ -32,6 +53,8 @@ public class Multi {
             }
             System.out.println();
         }
+        System.out.println();
+        System.out.println("Max: " + generalMax + " at " + maxI + "," + maxJ);
     }
 
     private int getMax(int nums[]){
@@ -44,21 +67,15 @@ public class Multi {
         return max;
     }
 
-    private void initMatrix(){
+    private int[][] initMatrix(String[] sArray, String[] tArray){
+        int[][] matrix = new int[sArray.length+1][tArray.length+1];
         for(int i = 0; i < matrix[0].length; i++){
             matrix[0][i] = 0;
         }
         for(int i = 0; i < matrix.length; i++){
             matrix[i][0] = 0;
         }
-
-        for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[0].length; j++){
-                //  System.out.println(i+","+j + ":"+ matrix[i][j] + " ");
-            }
-            //System.out.println();
-        }
-
+        return matrix;
     }
 
     private int calculateNorthOrWest(int value){
@@ -66,10 +83,41 @@ public class Multi {
     }
 
     private int calculateNorthWest(int value, String s, String t){
-        if (s == t || s == "?" || t == "?"){
+        if (s.equals(t) || s.equals("?") || t.equals("?")){
             return value + 1;
         }else{
             return value -1;
         }
+    }
+
+    private String readFile(String filePath){
+        String finalString = "";
+        try {
+            String line = "";
+
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader = new FileReader(filePath);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                finalString = finalString + line;
+            }
+
+            // Always close files.
+            bufferedReader.close();
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                    "Unable to open file");
+        }
+        catch(IOException ex) {
+            System.out.println(
+                    "Error reading file ");
+            // Or we could just do this:
+            // ex.printStackTrace();
+        }
+        return finalString;
     }
 }
